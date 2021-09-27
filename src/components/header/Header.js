9f381bd3ea16ed38b0af0d015871799ecb04247b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-27 00:20:16 -0300

    Se arma header

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const HeaderComponent = () => {
+    return (
+        <header className='header'>
+            <h1 className='title'>TecnoGame</h1>
+        </header>
+    );
+};
+
+export default HeaderComponent;
