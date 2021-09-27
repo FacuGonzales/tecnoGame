d7028fd64b21a4ed3298caa7bc76265ab42d8ce4
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-27 00:22:05 -0300

    Se declara componentes en app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import NavBar from './components/navbar/NavBar';
+import HeaderComponent from './components/header/Header';
+import LandingComponent from './components/landing/Landing';
+
+class App extends Component{
+
+  state = {
+    consola: 'PLAYSTATION',
+    precio: 10
+  }
+
+  cambiarConsola = ()=>{
+    console.log('object')
+    if(this.state.consola === 'PLAYSTATION'){
+      this.setState({consola: 'XBOX'})
+    }
+    else{
+      this.setState({consola: 'PLAYSTATION'})
+    }
+  }
+
+  render(){
+    return (
+      <>
+        <div className="headerContainer">
+          <HeaderComponent/>
+        </div>
+
+        <div className="bodyContainer">
+          <div className="navBarContainer">
+            <NavBar/>
+          </div>
+
+          <div className="landingContainer">
+            <LandingComponent/>
+          </div>
+        </div>
+      </>
+    );
+
+  }
 }
 
 export default App;
