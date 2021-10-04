0d7ccab37e4658cf89b11889f46a4cb17fe36988
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-04 20:46:23 -0300

    Se agrega botonera

--- src/components/itemListContainer/ItemCount/ItemCount.js
+++ src/components/itemListContainer/ItemCount/ItemCount.js
@@ -1,15 +1,21 @@
 
 import './ItemCount.css'
+import React from 'react';
+import { Icon } from 'semantic-ui-react'
 
 
-const ItemCount = ({stock, initial}) => {
-    console.log(initial)
+function ItemCount({stock, initial, setInitial}){
+    
     return (
         <>
             <div>
+                <Icon name='minus circle' onClick = {() => setInitial( initial > 0 ? initial -1 : 0)}/>
+
                
 
                 <p>Cantidad seleccionada: {initial}</p>
+
+                <Icon name='plus circle' onClick = {() => setInitial( initial < 10 ? initial + 1 : 10)}/>
 
                 
             </div>
