bf86fb50ce6a6ecd94e274bd1415abdc223cf131
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-06 21:22:48 -0300

    Se arma componente itemList

---
+++ src/components/itemListContainer/itemList/itemList.js
@@ -0,0 +1,81 @@
+import './itemList.css';
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+let productos = [
+    // {
+    //     teclados : [
+            {
+                id: 1,
+                nombre: 'Teclado Mecanico Soul Gaming',
+                precio: 6300,
+                url_image: 'https://firebasestorage.googleapis.com/v0/b/tecnogame-1101.appspot.com/o/soul.png?alt=media&token=40a127d1-a50c-4285-bc07-aa1ea7c01d26',
+                stock: 12,
+                oferta: false,
+                precio_oferta: null,
+            },
+            {
+                id: 2,
+                nombre: 'Teclado Gamer Hyperx Alloy Elite 2',
+                precio: 17643,
+                url_image: 'https://firebasestorage.googleapis.com/v0/b/tecnogame-1101.appspot.com/o/hyperex.png?alt=media&token=404fec16-2269-4cdc-a5e2-e868c4d4028d',
+                stock: 10,
+                oferta: false,
+                precio_oferta: null,
+            },
+            {
+                id: 1,
+                nombre: 'Teclado Gamer RGB Nisuta NSKBGZ88 QWERTY',
+                precio: 6899,
+                url_image: 'https://firebasestorage.googleapis.com/v0/b/tecnogame-1101.appspot.com/o/nisuta.png?alt=media&token=f02782cb-04e9-49b3-8404-1d55be4cfb03',
+                stock: 9,
+                oferta: false,
+                precio_oferta: null,
+            }
+    //     ]
+    // }
+    
+]
+
+
+const ItemList = ({title}) => {
+    const [ initial, setInitial] = React.useState(1);
+
+    return ( 
+        <>
+            <h4 className="h4Titulo">{title}</h4>
+            <ul className="ulProductos">
+                { 
+                 
+                    productos.map( p => 
+                        <li>
+                            <img src={p.url_image}/>
+                            <h3>{p.nombre}</h3>
+                            <p>$ {p.precio}</p>
+                         
+                            <div>
+                                <Icon name='minus circle' onClick = {() => setInitial( initial > 0 ? initial -1 : 0)}/>
+
+                                <p>{initial}</p>
+
+                                <Icon name='plus circle' onClick = {() => setInitial( initial < 10 ? initial + 1 : 10)}/>
+                            </div>
+
+
+                            <div class="ui buttons">
+                                <button class="ui button">Detalle</button>
+                                <div class="or"></div>
+                                <button class="ui positive button">Comprar</button>
+                            </div>
+
+                            <p>Stock Disponible: {p.stock}</p>
+                        </li>
+                    )
+               
+                }
+            </ul>
+        </>
+    )
+};
+
+export default ItemList;
