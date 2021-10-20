ab14c2b7736c8a73822405210921f8ad1baabef4
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-20 20:31:04 -0300

    Se agrega visualizacion de rutas

--- src/components/Details/ItemDetail/ItemDetail.js
+++ src/components/Details/ItemDetail/ItemDetail.js
@@ -1,33 +1,63 @@
-import { Button } from 'semantic-ui-react';
+// import { Button } from 'semantic-ui-react';
+import { useState } from 'react';
+import ItemCount from '../../itemListContainer/ItemCount/ItemCount';
+import LoadingComponent from '../../LoadingContainer/LoadingComponent';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import CartContainer from '../../CartContainer/CartContainer';
+
 
 const ItemDetail = ({item, setOpen}) => {
-    return (
-        <div class="ui items">
-            <div class="item">
-                <div class="image">
-                    <img src={item?.url_image}/>
-                </div>
-                
-                <div class="content">
-                    <div class="header">{item?.nombre}</div>
-                    
-                    <div class="meta">${item?.precio}</div>
-                    
-                    <div class="description">{item?.descripcion}</div>
+    const [ initial, setInitial] = useState(1);
 
-                    <div class="meta">Stock disponible: {item?.stock}</div>
-                    
-                    <div class="extra">
-                        <Button.Group>
-                            <Button onClick={() => setOpen( op => !op)}>Volver</Button>
-                            <Button.Or />
-                            <Button positive>Agregar al Carrito</Button>
-                        </Button.Group>
+    function addToCart(cantidad){
+
+        if(!item?.stock || item?.stock < cantidad) return console.log('No hay elementos', cantidad); 
+
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/carrito" exact>
+                        <CartContainer/>
+                    </Route>
+                </Switch>
+            </Router>
+        )
+
+    }
+
+    if(!item){
+        return <LoadingComponent></LoadingComponent>
+    }else{
+        return (
+            <div className="ui items">
+                <div className="item">
+                    <div className="image">
+                        <img src={item?.url_image}/>
+                    </div>
+    
+                    <div className="content">
+                        <div className="header">{item?.nombre}</div>
+    
+                        <div className="meta">${item?.precio}</div>
+    
+                        <div className="description">{item?.descripcion}</div>
+    
+                        <div className="meta">Stock disponible: {item?.stock}</div>
+    
+                        <div>
+                            <ItemCount stock={item?.stock} 
+                                        initial={initial}
+                                        setInitial={setInitial}
+                                        setOpen={setOpen}
+                                        onConfirm={addToCart}/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+
 };
 
 export default ItemDetail;
