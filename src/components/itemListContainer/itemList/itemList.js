40a056e7356903154fbc6f6fe8aace65fc919a27
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-20 14:53:32 -0300

    Se agrega loading al listado

--- src/components/itemListContainer/itemList/itemList.js
+++ src/components/itemListContainer/itemList/itemList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Item from '../Item/Item.js';
 
 import ProductosData from '../../../json/productos.json';
+import LoadingComponent from '../../LoadingContainer/LoadingComponent';
 
 const ItemList = ({title}) => {
     const [ prodList, setProdList ] = React.useState([]);
@@ -19,15 +20,19 @@ const ItemList = ({title}) => {
         });
     }, []);
 
-    return (
-        <>
-            <h2 className="h2Titulo">{title}</h2>
-
-            <div className="itemsList">
-                { prodList.map((item)=> <Item item={item} key={item.id} /> )}
-            </div>
-        </>
-    )
+    if(!prodList.length){
+        return <LoadingComponent></LoadingComponent>
+    }else{
+        return (
+            <>
+                <h2 className="h2Titulo">{title}</h2>
+    
+                <div className="itemsList">
+                    { prodList.map((item)=> <Item item={item} key={item.id} /> )}
+                </div>
+            </>
+        )
+    }
 };
 
 export default ItemList;
