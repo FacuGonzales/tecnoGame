ed8685acceda63efd582bb01220b1af2540692fe
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-20 15:37:34 -0300

    Se crea componente cart

---
+++ src/components/CartContainer/CartContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartContainer = () => {
+    return (
+        <section>
+            <h1>Tu Carrito</h1>
+        </section>
+    )
+};
+
+export default CartContainer;
