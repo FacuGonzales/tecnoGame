19de4ef621e6f45a61d20db9635d58951cc3466c
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-21 17:29:22 -0300

    Se modifica clases de count y datos

--- src/components/itemListContainer/ItemCount/ItemCount.js
+++ src/components/itemListContainer/ItemCount/ItemCount.js
@@ -1,34 +1,53 @@
-
-import './ItemCount.css'
 import React from 'react';
 import { Icon, Button } from 'semantic-ui-react';
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 
-function ItemCount({stock, initial, setInitial, setOpen, onConfirm}){
+function ItemCount({prod, initial, setInitial, onConfirm}){
     
     return (
         
         <Router>
+
             <div className="contadorContainer">
-                <Icon name='minus circle' onClick = {() => setInitial( initial > 0 ? initial -1 : 0)}/>
 
-                <p>{initial}</p>
+                <div className="contadorContainer--infoContainer">
+                    <div className="contadorContainer--infoContainer__stockContainer">
+                        <p className="contadorContainer--infoContainer__stockContainer--valor">Stock disponible: {prod.stock}</p>
+                    </div>
+                    
+                    <div className="contadorContainer--infoContainer__botonAgregarRestar">
+                        <Icon name='minus circle' 
+                              className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon"
+                              onClick = {() => setInitial( initial > 0 ? initial -1 : 0)}/>
 
-                <Icon name='plus circle' onClick = {() => setInitial( initial < 10 ? initial + 1 : 10)}/>
+                        <p className="contadorContainer--infoContainer__botonAgregarRestar--valorInicial">{initial}</p>
 
+                        <Icon name='plus circle' 
+                              className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon"
+                              onClick = {() => setInitial( initial < 10 ? initial + 1 : 10)}/>
+                    </div>
+                </div>
+
+                <div className="contadorContainer--botonesExtra">
+                    <Button.Group className="contadorContainer--botonesExtra__contenedorBotones">
+                        <Link to="/">
+                            <Button className="contadorContainer--botonesExtra__contenedorBotones--btnVolver">Volver</Button>
+                        </Link>
+
+                        <Button.Or className="contadorContainer--botonesExtra__contenedorBotones--btnOr"/>
+
+                        <Link to="/carrito">
+                            <Button positive 
+                                    className="contadorContainer--botonesExtra__contenedorBotones--btnAdd"
+                                    onClick={onConfirm(initial, prod)}>
+                                Agregar al Carrito
+                            </Button>
+                        </Link>
+                    </Button.Group>
+                </div>
             </div>
 
-            <p>Stock disponible: {stock}</p>
-
-            <div className="extra">
-                <Button.Group>
-                    <Button onClick={() => setOpen( op => !op)}>Volver</Button>
-                    <Button.Or />
-                    <Link to="/carrito">
-                        <Button positive onClick={() => { setOpen( op => !op); onConfirm(initial)}}>Agregar al Carrito</Button>
-                    </Link>
-                </Button.Group>
-            </div>
+            
         </Router>
         
     )
