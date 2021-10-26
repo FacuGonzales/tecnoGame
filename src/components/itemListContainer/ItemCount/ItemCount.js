db04361015a0252c8c8266974936ef169da148fd
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-10-26 11:11:49 -0300

    Se modifica funcionamiento de contador

--- src/components/itemListContainer/ItemCount/ItemCount.js
+++ src/components/itemListContainer/ItemCount/ItemCount.js
@@ -1,55 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
 import { Icon, Button } from 'semantic-ui-react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function ItemCount({prod, initial, setInitial, onConfirm}){
+function ItemCount({stock, initial, onClick}){
+
+    const [contador,setContador] = useState(parseInt(initial));
+
+    const masCantidad = ()=>{
+        if (contador < stock) setContador(contador + 1)
+    };
     
+    const menosCantidad = ()=>{
+        if (contador > 0 ) setContador(contador - 1)
+    };
+
     return (
-        
-        <Router>
-
-            <div className="contadorContainer">
-
-                <div className="contadorContainer--infoContainer">
-                    <div className="contadorContainer--infoContainer__stockContainer">
-                        <p className="contadorContainer--infoContainer__stockContainer--valor">Stock disponible: {prod.stock}</p>
-                    </div>
-                    
-                    <div className="contadorContainer--infoContainer__botonAgregarRestar">
-                        <Icon name='minus circle' 
-                              className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon"
-                              onClick = {() => setInitial( initial > 0 ? initial -1 : 0)}/>
-
-                        <p className="contadorContainer--infoContainer__botonAgregarRestar--valorInicial">{initial}</p>
-
-                        <Icon name='plus circle' 
-                              className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon"
-                              onClick = {() => setInitial( initial < 10 ? initial + 1 : 10)}/>
-                    </div>
+        <div className="contadorContainer">
+            <div className="contadorContainer--infoContainer">
+                <div className="contadorContainer--infoContainer__stockContainer">
+                    <p className="contadorContainer--infoContainer__stockContainer--valor">Stock disponible: {stock}</p>
                 </div>
 
-                <div className="contadorContainer--botonesExtra">
-                    <Button.Group className="contadorContainer--botonesExtra__contenedorBotones">
-                        <Link to="/">
-                            <Button className="contadorContainer--botonesExtra__contenedorBotones--btnVolver">Volver</Button>
-                        </Link>
-
-                        <Button.Or className="contadorContainer--botonesExtra__contenedorBotones--btnOr"/>
-
-                        <Link to="/carrito">
-                            <Button positive 
-                                    className="contadorContainer--botonesExtra__contenedorBotones--btnAdd"
-                                    onClick={onConfirm(initial, prod)}>
-                                Agregar al Carrito
-                            </Button>
-                        </Link>
-                    </Button.Group>
+                <div className="contadorContainer--infoContainer__botonAgregarRestar">
+                    <Icon name='minus circle' 
+                          className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon"
+                          onClick={menosCantidad}/>
+                    <p className="contadorContainer--infoContainer__botonAgregarRestar--valorInicial">{contador}</p>
+                    <Icon name='plus circle' 
+                          className="contadorContainer--infoContainer__botonAgregarRestar--botonIcon"
+                          onClick={masCantidad}/>
                 </div>
             </div>
-
-            
-        </Router>
-        
+            <div className="contadorContainer--botonesExtra">
+                <Button.Group className="contadorContainer--botonesExtra__contenedorBotones">
+                    <Link to="/">
+                        <Button className="contadorContainer--botonesExtra__contenedorBotones--btnVolver">Volver</Button>
+                    </Link>
+                    <Button.Or className="contadorContainer--botonesExtra__contenedorBotones--btnOr"/>
+                    
+                    <Button positive 
+                            className="contadorContainer--botonesExtra__contenedorBotones--btnAdd"
+                            onClick={()=>onClick(contador)}>
+                        Agregar al Carrito
+                    </Button>
+                </Button.Group>
+            </div>
+        </div>
     )
 };
 
