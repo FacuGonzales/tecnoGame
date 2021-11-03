e137b96b117216401ac147e1f071e90935fb6052
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-11-03 13:27:28 -0300

    Se arma metodo para muestra de badge con cantidad de items

--- src/components/CardWidget/CardWidget.js
+++ src/components/CardWidget/CardWidget.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Icon } from 'semantic-ui-react'
+import { CartContext } from '../Context/CartContext';
+import { Badge } from '@mui/material';
+const CardWidget = () => {
+    const { items } = useContext(CartContext);
+    const [itemsCount, setItemsCount] = useState(0);
 
-const CardWidget = () => <Icon name='shopping cart' className="icono"/>;
+    useEffect(() => {
+        let cantidad = items.reduce((acc, item) => acc + item.cantidad, 0);
+        setItemsCount(cantidad);
+    }, [items])
+
+
+    return(
+        <Badge badgeContent={itemsCount} color="secondary">
+            <Icon name='shopping cart' className="icono"/>
+        </Badge>
+    );
+}
 
 export default CardWidget;
