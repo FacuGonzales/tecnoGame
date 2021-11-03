c00c7676c213578adda7bfd752b1f9addf91c018
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-11-03 14:49:04 -0300

    Se trabaja en estilos de carrito

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import { Link } from 'react-router-dom'
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Divider } from 'semantic-ui-react'
 import { CartContext } from '../Context/CartContext';
 import CartItem from './CartItem/CartItem';
 
@@ -8,35 +8,44 @@ const CartContainer = () => {
     const { items, totalItems, totalPrice, clear} = useContext(CartContext)
 
     return (
-        <div>
-            <h2 className="titulo">Tu carrito </h2>
+        <div className="cartContainer">
+            <h2 className="cartContainer--titleList">Tu carrito </h2>
+
+            <Divider horizontal><Icon disabled name='cart plus'/></Divider>
+
             {
                 items.length === 0 ? 
-                    <div>
-                        <h2>No hay productos agregados en el carrito</h2>
+                    <div className="cartContainer--sinComprasContainer">
+                        <h2 className="cartContainer--sinComprasContainer__titleSinCompras">No hay productos agregados en el carrito</h2>
 
                         <Link to="/">
-                            <Button positive >Continuar Comprando</Button>
+                            <Button positive className="cartContainer--sinComprasContainer__button">Continuar Comprando</Button>
                         </Link>
 
                     </div>
                         
                 :
                 
-                <div>
+                <div className="cartContainer--comprasContainer">
                     <CartItem/>
 
-                    <p>Total de Items{ totalItems() }</p>
+                    <div className="cartContainer--comprasContainer__infoTotal">
+                        <p className="cartContainer--comprasContainer__infoTotal--totalPrice">TOTAL: USD { totalPrice() } | </p>
 
-                    <p>TOTAL A PAGAR: USD { totalPrice() }</p>
-                   
-                    <Button.Group>
-                        <Button onClick={ () => clear() }><Icon name='delete' />Vacíar Carrito</Button>
+                        <p className="cartContainer--comprasContainer__infoTotal--totalItems"> x { totalItems() } Unidades</p>
+                    </div>
+                    
+                    <Button.Group className="cartContainer--comprasContainer__buttonsContainer">
+                        <Button onClick={ () => clear() } className="cartContainer--comprasContainer__buttonsContainer--vaciarButton">
+                            <Icon name='delete' className="cartContainer--comprasContainer__buttonsContainer--vaciarButton__icon"/>Vacíar Carrito
+                        </Button>
 
                         <Button.Or />
 
                         <Link to="/">
-                            <Button positive><Icon name='shop'/>Continuar Comprando</Button>
+                            <Button positive className="cartContainer--comprasContainer__buttonsContainer--continuarButton">
+                                <Icon name='shop'className="cartContainer--comprasContainer__buttonsContainer--continuarButton__icon"/>Continuar Comprando
+                            </Button>
                         </Link>
                     </Button.Group>
                     
--- src/components/CartContainer/CartItem/CartItem.js
+++ src/components/CartContainer/CartItem/CartItem.js
@@ -1,6 +1,11 @@
 import React,{useContext} from 'react';
 import { CartContext } from '../../Context/CartContext';
-import { Button, Image, List, Icon } from 'semantic-ui-react'
+import { Divider, Button, Image, Icon, List } from 'semantic-ui-react'
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
 const CartItem = () => {
 
@@ -12,18 +17,52 @@ const CartItem = () => {
                 items.map(i=>{
                     return(
 
-                        <List divided verticalAlign='middle'>
+                        // <List sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}>
+                        //     <ListItem>
+                        //         <ListItemAvatar>
+                        //             <Avatar>
+                        //                 <Image avatar src={i.url_image}/>
+                        //             </Avatar>
+                        //         </ListItemAvatar>
+                        //         <ListItemText primary={i.nombre} secondary={i.precio} />
+                        //     </ListItem>
+                        // </List>
+<>
+                        <List divided verticalAlign='middle' className="carItemList">
         
-                            <List.Item>
-                                <List.Content floated='right'>
-                                    <Button onClick={()=>{removeItem(i.id)}}><Icon name='trash'/></Button>
+                            <List.Item className="carItemList__item">
+                                
+
+                                {/* <Image avatar src={i.url_image}/> */}
+                                <div className="carItemList__item--infoContainer">
+                                    <ListItemAvatar className="carItemList__item--infoContainer__avatarContainer">
+                                        <Avatar className="carItemList__item--infoContainer__avatarContainer__avatar">
+                                            <Image avatar src={i.url_image} className="carItemList__item--infoContainer__avatarContainer__avatar--img"/>
+                                        </Avatar>
+                                    </ListItemAvatar>
+
+                                    <List.Content className="carItemList__item--infoContainer__datos">
+                                        <h2 className="carItemList__item--infoContainer__datos--tituloProd">{i.nombre}</h2>
+                                    </List.Content>
+                                </div>
+
+                                <List.Content floated='right' className="carItemList__item--buttonContainer"> 
+
+                                    <p className="carItemList__item--buttonContainer__precioContainer">USD {i.precio} -</p>
+
+                                    <p className="carItemList__item--buttonContainer__cantidadContainer"> x{i.cantidad} Uni.</p>
+                                   
+                                    <Button onClick={()=>{removeItem(i.id)}} className="carItemList__item--buttonContainer__button">
+                                        <Icon name='trash'className="carItemList__item--buttonContainer__button--icon"/>
+                                    </Button>
                                 </List.Content>
 
-                                <Image avatar src={i.url_image}/>
-
-                                <List.Content>{i.nombre} - USD {i.precio} - Cantidad: {i.cantidad}</List.Content>
                             </List.Item>
+                        
+                            <Divider horizontal><Icon disabled name='cart plus'/></Divider>
                         </List>
+
+                        </>
                     )
                 })
             }
